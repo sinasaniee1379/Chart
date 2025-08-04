ce7f0e06d2a1e23db3907076808ffadbca14581f
sina saniee <sinasan67@gmail.com>
2025-08-04 09:46:14 +0330

    feat: remove logs

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,14 +1,11 @@
 import { FC } from "react";
-import * as d3 from "d3";
 import { IChartProps } from "@/types";
 import MultiSeriesChart from "./MultiSeriesChart";
 import SingleSeriesChart from "./SingleSeriesChart";
 
 const Chart: FC<IChartProps> = ({ chart }) => {
-  console.log({ chart });
   const firstValue = chart.data.find((d) => d[1] !== null)?.[1];
   const isMultiSeries = Array.isArray(firstValue);
-  console.log({ firstValue, isMultiSeries });
   return (
     <div
       style={{
